bef34b04907ff810ffacc94e512605d65745d0be
Yesilyurt, Cihan <cihan.yesilyurt@siemens.com>
2016-06-15 16:54:20 +0300

    menu for "about us, preferences" added

--- app/src/main/java/birbit/thenewboston/Menu.java
+++ app/src/main/java/birbit/thenewboston/Menu.java
@@ -3,6 +3,7 @@ package birbit.thenewboston;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -35,5 +36,13 @@ public class Menu extends ListActivity{
         }catch(ClassNotFoundException e){
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(android.view.Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater blowUp = getMenuInflater();
+        blowUp.inflate(R.menu.cool_menu,menu);
+        return true;
     }
 }
