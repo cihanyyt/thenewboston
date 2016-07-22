23e42438fe84bfd30f497042089f99d1c72ab758
Yesilyurt, Cihan (CT DD DS EU TR DF FA-2) <cihan.yesilyurt@siemens.com>
2016-07-22 14:56:09 +0300

    tutorial 73

--- app/src/main/java/birbit/thenewboston/GFXSurface.java
+++ app/src/main/java/birbit/thenewboston/GFXSurface.java
@@ -17,8 +17,8 @@ import android.view.View;
 public class GFXSurface extends Activity implements View.OnTouchListener {
 
     MyBringBackSurface ourSurfaceView;
-    float x,y;
-
+    float x, y, sX, sY, fX, fY, dX, dY, aniX, aniY, scaX, scaY;
+    Bitmap test, plus;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +27,14 @@ public class GFXSurface extends Activity implements View.OnTouchListener {
         ourSurfaceView.setOnTouchListener(this);
         x = 0;
         y = 0;
+        sX = 0;
+        sY = 0;
+        fX = 0;
+        fY = 0;
+        dX = dY = aniX = aniY = scaX = scaY = 0;
+
+        test = BitmapFactory.decodeResource(getResources(),R.drawable.monster);
+        plus = BitmapFactory.decodeResource(getResources(),R.drawable.monster_selected);
 
         setContentView(ourSurfaceView);
     }
@@ -47,6 +55,23 @@ public class GFXSurface extends Activity implements View.OnTouchListener {
     public boolean onTouch(View v, MotionEvent event) {
         x = event.getX();
         y = event.getY();
+
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                sX = event.getX();
+                sY = event.getY();
+                dX = dY = aniX = aniY = scaX = scaY = fX = fY = 0;
+                break;
+            case MotionEvent.ACTION_UP:
+                fX = event.getX();
+                fY = event.getY();
+                dX = fX - sX;
+                dY = fY - sY;
+                scaX = dX/30;
+                scaY = dY/30;
+                x = y = 0;
+                break;
+        }
 
         return true;
     }
@@ -91,10 +116,20 @@ public class GFXSurface extends Activity implements View.OnTouchListener {
 
                 Canvas canvas = ourHolder.lockCanvas();
                 canvas.drawRGB(02,02,150);
+
                 if(x != 0 && y != 0){
-                    Bitmap test = BitmapFactory.decodeResource(getResources(),R.drawable.monster);
                     canvas.drawBitmap(test, x-test.getWidth()/2, y-test.getHeight()/2, null);
                 }
+                if(sX != 0 && sY != 0){
+                    canvas.drawBitmap(plus, sX-plus.getWidth()/2, sY-plus.getHeight()/2, null);
+                }
+                if(fX != 0 && fY != 0){
+                    canvas.drawBitmap(test, fX-(test.getWidth()/2)-aniX, fY-(test.getHeight()/2)-aniY, null);
+                    canvas.drawBitmap(plus, fX-(plus.getWidth()/2), fY-(plus.getHeight()/2), null);
+                }
+                aniX += scaX;
+                aniY += scaY;
+
                 ourHolder.unlockCanvasAndPost(canvas);
             }
 
