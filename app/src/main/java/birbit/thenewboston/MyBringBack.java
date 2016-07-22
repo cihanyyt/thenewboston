e097c0207a939140c6f824b65a635537da648441
Yesilyurt, Cihan (CT DD DS EU TR DF FA-2) <cihan.yesilyurt@siemens.com>
2016-07-22 13:21:46 +0300

    tutorial 66

--- app/src/main/java/birbit/thenewboston/MyBringBack.java
+++ app/src/main/java/birbit/thenewboston/MyBringBack.java
@@ -7,6 +7,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.graphics.Typeface;
 import android.view.View;
 
 /**
@@ -17,17 +18,27 @@ public class MyBringBack extends View{
     Bitmap gBall;
     float changingY;
 
+    Typeface font;
+
     public MyBringBack(Context context) {
         super(context);
 
         gBall = BitmapFactory.decodeResource(getResources(),R.drawable.monster);
         changingY = 0;
+        font = Typeface.createFromAsset(context.getAssets(),"FFF_Tusj.ttf");
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         canvas.drawColor(Color.WHITE);
+
+        Paint textPaint = new Paint();
+        textPaint.setARGB(50, 255, 10, 50);
+        textPaint.setTextAlign(Paint.Align.CENTER);
+        textPaint.setTextSize(50);
+        canvas.drawText("mybringback", canvas.getWidth()/2, 200, textPaint);
+
         canvas.drawBitmap(gBall,(canvas.getWidth()/2), changingY, null);
         if(changingY < canvas.getHeight()){
             changingY += 10;
