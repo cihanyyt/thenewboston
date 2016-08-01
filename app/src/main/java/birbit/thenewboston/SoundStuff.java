109f0338a1d917d126725fd960ca729e7eabb157
Yesilyurt, Cihan (CT DD DS EU TR DF FA-2) <cihan.yesilyurt@siemens.com>
2016-08-01 16:27:43 +0300

    tutorial 80

--- app/src/main/java/birbit/thenewboston/SoundStuff.java
+++ app/src/main/java/birbit/thenewboston/SoundStuff.java
@@ -2,6 +2,7 @@ package birbit.thenewboston;
 
 import android.app.Activity;
 import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.media.SoundPool;
 import android.os.Bundle;
 import android.view.View;
@@ -9,10 +10,11 @@ import android.view.View;
 /**
  * Created by Z003EUNW on 8/1/2016.
  */
-public class SoundStuff extends Activity implements View.OnClickListener {
+public class SoundStuff extends Activity implements View.OnClickListener, View.OnLongClickListener {
 
     SoundPool soundPool;
-    int exp;
+    int exp = 0;
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,14 +22,23 @@ public class SoundStuff extends Activity implements View.OnClickListener {
 
         View v = new View(this);
         v.setOnClickListener(this);
+        v.setOnLongClickListener(this);
         setContentView(v);
         soundPool = new SoundPool(5, AudioManager.STREAM_MUSIC, 0);
         exp = soundPool.load(this, R.raw.wasp, 1);
+        mediaPlayer = MediaPlayer.create(this,R.raw.music);
     }
 
     @Override
     public void onClick(View v) {
         if(exp != 0)
             soundPool.play(exp, 1, 1, 0, 0,1);
+    }
+
+
+    @Override
+    public boolean onLongClick(View v) {
+        mediaPlayer.start();
+        return false;
     }
 }
