5157cafe80858a7661873ac2e1cd95b0ef593f69
Yesilyurt, Cihan (CT DD DS EU TR DF FA-2) <cihan.yesilyurt@siemens.com>
2016-08-01 16:45:10 +0300

    tutorial 82

---
+++ app/src/main/java/birbit/thenewboston/Slider.java
@@ -0,0 +1,15 @@
+package birbit.thenewboston;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Z003EUNW on 8/1/2016.
+ */
+public class Slider extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sliding);
+    }
+}
