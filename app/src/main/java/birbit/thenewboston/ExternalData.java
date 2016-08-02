7ae01a6ead6cc51bf465d891176c15f7953bdb27
Yesilyurt, Cihan (CT DD DS EU TR DF FA-2) <cihan.yesilyurt@siemens.com>
2016-08-02 15:01:20 +0300

    tutorial 110

--- app/src/main/java/birbit/thenewboston/ExternalData.java
+++ app/src/main/java/birbit/thenewboston/ExternalData.java
@@ -1,6 +1,8 @@
 package birbit.thenewboston;
 
 import android.app.Activity;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.View;
@@ -11,8 +13,14 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.SpinnerAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 /**
  * Created by Z003EUNW on 8/2/2016.
@@ -25,6 +33,7 @@ public class ExternalData extends Activity implements AdapterView.OnItemSelected
     Spinner spinner;
     String[] paths = {"Music", "pictures", "Download"};
     File path = null;
+    File file = null;
     EditText saveFile;
     Button confirm, save;
 
@@ -39,9 +48,19 @@ public class ExternalData extends Activity implements AdapterView.OnItemSelected
         save = (Button) findViewById(R.id.bSaveFile);
         confirm.setOnClickListener(this);
         save.setOnClickListener(this);
+        saveFile = (EditText) findViewById(R.id.etSaveAs);
 
-        canWrite.setText("false");
-        canRead.setText("false");
+        checkState();
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(ExternalData.this,
+                android.R.layout.simple_spinner_item, paths);
+        spinner = (Spinner) findViewById(R.id.spinner);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
+    }
+
+    private void checkState() {
         state = Environment.getExternalStorageState();
         if(state.equals(Environment.MEDIA_MOUNTED)){
             // read n write
@@ -60,13 +79,6 @@ public class ExternalData extends Activity implements AdapterView.OnItemSelected
             canR = false;
             canW = true;
         }
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(ExternalData.this,
-                android.R.layout.simple_spinner_item, paths);
-        spinner = (Spinner) findViewById(R.id.spinner);
-        spinner.setAdapter(adapter);
-        spinner.setOnItemSelectedListener(this);
-
     }
 
     @Override
@@ -95,7 +107,42 @@ public class ExternalData extends Activity implements AdapterView.OnItemSelected
     public void onClick(View v) {
         switch(v.getId()){
             case R.id.bSaveFile:
+                String f = saveFile.getText().toString();
+                file = new File(path, f + ".png");
 
+                checkState();
+                if(canW == canR == true) {
+
+                    path.mkdirs();
+                    try {
+                        InputStream is = getResources().openRawResource(R.drawable.monster);
+                        OutputStream os = new FileOutputStream(file);
+                        byte[] data = new byte[is.available()];
+                        is.read(data);
+                        os.write(data);
+                        is.close();
+                        os.close();
+
+                        Toast t = Toast.makeText(ExternalData.this, "File has been saved", Toast.LENGTH_LONG);
+                        t.show();
+                        // update files for user to use
+                        MediaScannerConnection.scanFile(ExternalData.this,
+                                new String[]{file.toString()},
+                                null,
+                                new MediaScannerConnection.OnScanCompletedListener() {
+                                    @Override
+                                    public void onScanCompleted(String path, Uri uri) {
+                                        Toast t = Toast.makeText(ExternalData.this, "scan complete", Toast.LENGTH_SHORT);
+                                        t.show();
+                                    }
+                                });
+
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
                 break;
             case R.id.bConfirmSaveAs:
                 save.setVisibility(View.VISIBLE);
